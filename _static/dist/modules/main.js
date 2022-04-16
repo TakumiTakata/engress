fce1e9136e6a32d73e92d40f5aee2b264fded747
takumi <t.takata121@outlook.jp>
2022-04-16 18:36:41 +0900

    csss調整

--- _static/dist/modules/main.js
+++ _static/dist/modules/main.js
@@ -4,6 +4,6 @@ $(".js-ham-button").click(function () {
 });
 
 $(".js-question").click(function() {
-  $(this).next(".js-answer").addClass('test');
+  $(this).children().toggleClass('active');
   $(this).next(".js-answer").fadeToggle();
 });
